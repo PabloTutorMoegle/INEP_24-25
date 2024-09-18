a27942d15681c6499f9efddd5b3bd61aec6d62fd
PabloTutorMoegle <pablotutormoegle@gmail.com>
2024-09-18 19:11:07 +0200

    update

--- INEP_Sesion1/Menu.cpp
+++ INEP_Sesion1/Menu.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+void procesarRegistroUsuario()
+{
+    string alias, nombre;
+    cout << "Escriba su alias: ";
+    cin >> alias;
+    cout << "Escriba su nombre completo: ";
+    cin >> nombre;
+
+    cout << "El registre de l'usuari " << nombre << " (" << alias << ") " << 
+            "s'ha processat correctament" << endl;
+}
 void GestionUsuarios()
 {
     cout << "1. Gestió usuari" << "\n" <<
@@ -13,7 +24,29 @@ void GestionUsuarios()
             "   5. Tornar" << endl;
     int numGU;
     cout << "Escriba uno de los numero del menu Gestió usuari" << endl; 
-    cin >> numGU;
+    while (cin >> numGU && numGU != 5)
+    {
+        if (numGU == 1)
+        {
+            procesarRegistroUsuario();
+        }
+        else if (numGU > 5)
+        {
+            cout << "Ese numero no es valido." << endl;
+        }
+        else 
+        {
+            cout << "Algo pasa al clickar el " << numGU << endl;
+        }
+        cout << "1. Gestió usuari" << "\n" <<
+                "   1. Registre usuari" << "\n" <<
+                "   2. Consulta usuari" << "\n" <<
+                "   3. Modifica usuari" << "\n" <<
+                "   4. Esborra usuari" << "\n" <<
+                "   5. Tornar" << endl;
+        cout << "Escriba uno de los numero del menu Gestió usuari" << endl; 
+    }
+    cout << "-----------------------------" << endl;
 }
 void GestionContenidos()
 {
@@ -23,7 +56,23 @@ void GestionContenidos()
             "   3. Tornar" << endl;
     int numGC;
     cout << "Escriba uno de los numero del menu Gestió continguts" << endl; 
-    cin >> numGC;    
+    while (cin >> numGC && numGC != 3)
+    {
+        if (numGC > 3)
+        {
+            cout << "Ese numero no es valido." << endl;
+        }
+        else 
+        {
+            cout << "Algo pasa al clickar el " << numGC << endl;
+        }
+        cout << "2. Gestió continguts" << "\n" <<
+                "   1. Gestió pel·lícules" << "\n" <<
+                "   2. Gestió sèries" << "\n" <<
+                "   3. Tornar" << endl;
+        cout << "Escriba uno de los numero del menu Gestió continguts" << endl; 
+    }
+    cout << "-----------------------------" << endl;
 }
 void Consultas()
 {
@@ -34,7 +83,25 @@ void Consultas()
             "   4. Tornar" << endl;
     int numC;
     cout << "Escriba uno de los numero del menu Consultes" << endl; 
-    cin >> numC;
+    while (cin >> numC && numC != 4)
+    {
+        if (numC > 4)
+        {
+            cout << "Ese numero no es valido." << endl;
+        }
+        else 
+        {
+            cout << "Algo pasa al clickar el " << numC << endl;
+        }
+        cout << "3. Consultes" << "\n" <<
+                "   1. Consulta per qualificació d'edat" << "\n" <<
+                "   2. Últimes novetats" << "\n" <<
+                "   3. Pròximes estrenes" << "\n" <<
+                "   4. Tornar" << endl;
+
+        cout << "Escriba uno de los numero del menu Consultes" << endl; 
+    }
+    cout << "-----------------------------" << endl;
 }
 
 int main()
@@ -67,6 +134,12 @@ int main()
         {
             cout << "Numero no valido" << endl;
         }
+        cout << "-----------------------------" << endl;
+        cout << "1. Gestió usuaris" << "\n" <<
+                "2. Gestió continguts" << "\n" <<
+                "3. Consultes" << "\n" <<
+                "4. Sortir" << endl;
+        
         cout << "Escriba uno de los numero del menu" << endl; 
     }
     cout << "Bye bye" << endl;
