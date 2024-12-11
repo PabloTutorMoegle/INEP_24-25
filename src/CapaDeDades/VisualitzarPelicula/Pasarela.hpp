cbf98d6ceeae0929d5180de5a530da92259bdb6c
Ari Vidal <ariadna.vidal.i@estudiantat.upc.edu>
2024-12-11 19:28:02 +0100

    cercadora i pasarela pelicula fet

--- src/CapaDeDades/VisualitzarPelicula/Pasarela.hpp
+++ src/CapaDeDades/VisualitzarPelicula/Pasarela.hpp
@@ -1,1 +1,18 @@
-// TODO
+#include <iostream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+class PasarelaVisualitzaPelicula {
+    private: 
+        string _sobrenom;
+        string _titolPelicula;
+        int _numVisualitzacions;
+        time_t data;
+
+    public: 
+        void insereix();
+        void modifica();
+        void esborra();
+};
