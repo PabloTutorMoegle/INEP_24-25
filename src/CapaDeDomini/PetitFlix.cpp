0d2c0886cfa6b1c6aee7c7788dfae31adb7fe8be
Slushee <slushee@duck.com>
2024-12-13 20:01:56 +0100

    Implementar classe PetitFlix

--- src/CapaDeDomini/PetitFlix.cpp
+++ src/CapaDeDomini/PetitFlix.cpp
@@ -1,1 +1,32 @@
 #include "PetitFlix.hpp"
+
+PetitFlix* PetitFlix::_instance = nullptr;
+
+PetitFlix* PetitFlix::get_instance() {
+    if (_instance == nullptr) {
+        _instance = new PetitFlix();
+    }
+    return _instance;
+}
+
+PetitFlix::PetitFlix() {
+    _usuari = std::nullopt;
+}
+
+PasarelaUsuari PetitFlix::obte_usuari() {
+    if (_usuari) {
+        return *_usuari;
+    } else {
+        throw "No hi ha cap sessió iniciada";
+    }
+}
+
+// Pre: l'usuari ha estat autenticat previament
+// I sure do hope no one runs this function ouside of it's indended context
+void PetitFlix::inicia_sessio(PasarelaUsuari usuari) {
+    _usuari = usuari;
+}
+
+void PetitFlix::tanca_sessio() {
+    _usuari = std::nullopt;
+}
--- src/CapaDeDomini/PetitFlix.hpp
+++ src/CapaDeDomini/PetitFlix.hpp
@@ -1,1 +1,21 @@
-// TODO
+#include <optional>
+using std::optional;
+
+#include "../CapaDeDades/Usuari/Pasarela.hpp"
+
+class PetitFlix {
+    private:
+        static PetitFlix* _instance;
+
+        optional<PasarelaUsuari> _usuari;
+
+        PetitFlix();
+    
+    public:
+        static PetitFlix* get_instance();
+
+        PasarelaUsuari obte_usuari();
+        void inicia_sessio(PasarelaUsuari usuari);
+        void tanca_sessio();
+
+};
