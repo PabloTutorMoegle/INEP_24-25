f9ad9e6371c6728324f5b4283288d52aaf8bbaff
Slushee <55996847+Slushee-a@users.noreply.github.com>
2024-12-17 14:24:53 +0000

    Implementar PasarelaVisualitzarSerie

--- src/CapaDeDades/VisualitzarSerie/Cercadora.hpp
+++ src/CapaDeDades/VisualitzarSerie/Cercadora.hpp
@@ -1,6 +1,12 @@
 #include <string>
+using std::string;
+
+#include <memory>
+using std::unique_ptr;
 
 #include "Pasarela.hpp"
+#include "../ConnexioBDD.hpp"
+#include "../../definitions.hpp"
 
 class Cercadora {
     public:
--- src/CapaDeDades/VisualitzarSerie/Pasarela.hpp
+++ src/CapaDeDades/VisualitzarSerie/Pasarela.hpp
@@ -1,18 +1,40 @@
 #include <string>
 #include <time.h>
+using std::string;
+
+#include "../../definitions.hpp"
+#include "../ConnexioBDD.hpp"
+#include <cppconn/prepared_statement.h>
 
 class PasarelaVisualitzarSerie {
     private:
-        std::string _sobrenom;
-        std::string _titolSerie;
+        string _sobrenom;
+        string _titolSerie;
         int _numVicialitzacions;
         int _numTemporada;
         int _numCapitol;
         time_t _data;
+        ModalitatSubscripcio _modalitat_subscripcio;
 
     public:
-        PasarelaVisualitzarSerie();
+        PasarelaVisualitzarSerie(
+            string sobrenom, 
+            string titolSerie, 
+            int numVicialitzacions, 
+            int numTemporada, 
+            int numCapitol, 
+            time_t data,
+            ModalitatSubscripcio modalitat_subscripcio
+        );
         void Insereix();
         void Modifica();
         void Esborra();
+
+        string ObteSobrenom();
+        string ObteTitolSerie();
+        int ObteNumVicialitzacions();
+        int ObteNumTemporada();
+        int ObteNumCapitol();
+        time_t ObteData();
+        ModalitatSubscripcio ObteModalitatSubscripcio();
 };
