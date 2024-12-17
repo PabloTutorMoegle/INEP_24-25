a2ad24c861b5108b4e88c56082c7b11c645aa74a
Slushee <slushee@duck.com>
2024-12-17 15:04:42 +0100

    Afegir TxIniciSessio

--- src/CapaDeDomini/Transaccions/IniciSessio.hpp
+++ src/CapaDeDomini/Transaccions/IniciSessio.hpp
@@ -1,1 +1,16 @@
-// TODO
+#include <string>
+using std::string;
+
+#include "../PetitFlix.hpp"
+#include "../../CapaDeDades/Usuari/Cercadora.hpp"
+
+class TxIniciSessio {
+    private:
+        string _sobrenom_usuari;
+        string _contrasenya_usuari;
+
+    public:
+        TxIniciSessio(string sobrenom_usuari, string contrasenya_usuari);
+
+        void executar();
+};
